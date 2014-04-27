1402471afa0e8acbc1fb700c36b1fefeafbde7b3
qadmlee <qadmlee@gmail.com>
2014-04-27 12:11:31 +0800

    modified output

--- speed_test.py
+++ speed_test.py
@@ -5,12 +5,16 @@ import subprocess
 
 ping_times = 3
 
+speed_test_result = []
+
 class Server(object):
     def __init__(self,category,region,ip,usage):
         self.category = category
         self.region = region
         self.ip = ip
         self.usage = usage
+    def to_string():
+        print("server IP = " + self.ip)
 
 class PingReault(object):
     def __init(self,server,minimum,maximum,average):
@@ -25,9 +29,13 @@ class PingThread(threading.Thread):
         self.server = server
    
     def run(self):
-        output = subprocess.check_output("ping -c " + str(ping_times) + " " + server.ip + " | grep round-trip",shell=True)
-        result = output.split("=").split("\/")
-        print result
+        try:
+            output = subprocess.check_output("ping -c " + str(ping_times) + " " + self.server.ip + " | grep round-trip",shell=True)
+            result = output
+            print(self.server.ip + "\t" + result.split("=")[1])
+            speed_test_result.append(result)
+        except:
+            None
 
 if __name__ == "__main__":
     servers = []
@@ -44,3 +52,6 @@ if __name__ == "__main__":
     for server in servers:
         pingThread = PingThread(server)
         pingThread.start()
+
+    for test_result in speed_test_result:
+        print test_result
